3f7ea757e1f099ba2d425d13f1d28f9130d7caa8
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-03-08 06:02:57 -0500

    backend updates

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,29 +1,60 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { HomePage, LoginPage, DashboardPage, TicketsPage } from './pages';
-import { RequireAuth } from './context/Auth/useAuth';
+import {
+	Route,
+	Routes,
+	Navigate
+} from 'react-router-dom';
+import {
+	HomePage,
+	LoginPage,
+	DashboardPage,
+	TicketsPage
+} from './pages';
+import {
+	RequireAuth
+} from './context/auth/useAuth';
 
 /**
  * @description Handle all the routes
  */
 
 const Router = () => {
-	return (
-		<Routes>
-			<Route path="/" element={<HomePage />} />
-			<Route path="/login" element={<LoginPage />} />
-			<Route
-				path="/dashboard"
-				element={
-					<RequireAuth>
-						<DashboardPage />
-					</RequireAuth>
-				}
-			/>
-			<Route path="/tickets/:id" element={<TicketsPage />} />
+	return ( <
+		Routes >
+		<
+		Route path = "/"
+		element = {
+			< HomePage / >
+		}
+		/> <
+		Route path = "/login"
+		element = {
+			< LoginPage / >
+		}
+		/> <
+		Route path = "/dashboard"
+		element = {
+			<
+			RequireAuth >
+			<
+			DashboardPage / >
+			<
+			/RequireAuth>
+		}
+		/> <
+		Route path = "/tickets/:id"
+		element = {
+			< TicketsPage / >
+		}
+		/>
 
-			{/* default route */}
-			<Route path="*" element={<Navigate to="/" />} />
-		</Routes>
+		{
+			/* default route */ } <
+		Route path = "*"
+		element = {
+			< Navigate to = "/" / >
+		}
+		/> <
+		/Routes>
 	);
 };
 
