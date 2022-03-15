c172e29478d18f78ec16c96cd0718906fc7bc093
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-03-15 17:53:51 -0400

    adds sentry to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
+import {
+	BrowserTracing
+} from "@sentry/tracing";
+import {
+	BrowserRouter
+} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 import './scss/main.scss';
+
+Sentry.init({
+	dsn: "https://5eae7ca6e6c449aa966af69375b3eb82@o1168708.ingest.sentry.io/6260841",
+	integrations: [new BrowserTracing()],
+
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
 	<BrowserRouter>
