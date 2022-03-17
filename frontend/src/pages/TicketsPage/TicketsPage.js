93e8b6e00adea1ba64475a12a9b9a72d5a8c93af
Jaime Convery <jaimeconvery@gmail.com>
2022-03-17 14:01:06 -0400

    fix: sticky page and full height columns

--- frontend/src/pages/TicketsPage/TicketsPage.js
+++ frontend/src/pages/TicketsPage/TicketsPage.js
@@ -3,11 +3,27 @@ import React, { Fragment, useEffect } from 'react';
 import { PurchaseTickets } from '../../components';
 import Ticket from '../../components/Ticket/Ticket';
 
+import './ticketPage.scss';
+import { fullHeightContainer, removeFullHeightContainer } from './../../utilities/helper';
+import { useLocation } from 'react-router-dom';
+
 export default function TicketsPage() {
+	let location = useLocation();
+
+	useEffect(() => {
+		const el = document.querySelector('#wrapper').parentElement;
+
+		fullHeightContainer(location.pathname, el);
+
+		return () => {
+			removeFullHeightContainer(el);
+		};
+	}, []);
+
 	return (
-		<Fragment>
+		<div id="wrapper">
 			<Ticket />
 			<PurchaseTickets />
-		</Fragment>
+		</div>
 	);
 }
