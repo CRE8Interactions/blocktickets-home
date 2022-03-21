fe40e6d88546030044148f73b4e9f1db7cf15a7a
Jaime Convery <jaimeconvery@gmail.com>
2022-03-21 19:13:25 -0400

    fix: import error  - wrong folder name

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,35 +1,27 @@
-import {
-  Route,
-  Routes,
-  Navigate
-} from 'react-router-dom';
-import {
-  HomePage,
-  LoginPage,
-  DashboardPage,
-  TicketsPage
-} from './pages';
-import {
-  RequireAuth
-} from './context/auth/useAuth';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { HomePage, LoginPage, DashboardPage, TicketsPage } from './pages';
+import { RequireAuth } from './context/Auth/useAuth';
 
 /**
  * @description Handle all the routes
  */
 
 const Router = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />}/>{' '}
-      <Route path="/login" element={<LoginPage />}/>{' '}
-      <Route path="/dashboard" element={
-        <RequireAuth>
-          <DashboardPage />
-        </RequireAuth>}
-      />{' '} 
-      <Route path="/tickets/:id" element={<TicketsPage /> } /> {/* Add a NotFound route */}
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<HomePage />} />{' '}
+			<Route path="/login" element={<LoginPage />} />{' '}
+			<Route
+				path="/dashboard"
+				element={
+					<RequireAuth>
+						<DashboardPage />
+					</RequireAuth>
+				}
+			/>{' '}
+			<Route path="/tickets/:id" element={<TicketsPage />} /> {/* Add a NotFound route */}
+		</Routes>
+	);
 };
 
 export default Router;
