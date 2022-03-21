1894cda89accbc0b6066da8ed7110517d8769580
Jaime Convery <jaimeconvery@gmail.com>
2022-03-21 19:04:41 -0400

    refactor: add seating map

--- frontend/src/components/PurchaseTickets/SeatingMap/SeatingMap.js
+++ frontend/src/components/PurchaseTickets/SeatingMap/SeatingMap.js
@@ -2,18 +2,13 @@ import React, { Fragment } from 'react';
 
 import './seatingMap.scss';
 import generalAdmissionsMap from '../../../assets/map-general_admissions.svg';
+import seatingMap from '../../../assets/map-seating.svg';
 
 export default function SeatingMap({ styles, type }) {
 	return (
 		<div className={`map align-items-center justify-content-around ${styles && styles}`}>
 			<img
-				src={
-					type === 'genAdmissions' ? (
-						generalAdmissionsMap
-					) : (
-						'//mapsapi.tmol.io/maps/geometry/image/25/46/254607?removeFilters=ISM_Shadow&avertaFonts=true&app=CCP'
-					)
-				}
+				src={type === 'genAdmissions' ? generalAdmissionsMap : seatingMap}
 				alt="Seating Map"
 			/>
 		</div>
