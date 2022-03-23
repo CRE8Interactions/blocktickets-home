c8b3f5c21da2a8f969d8c2b0d75b027d0a077327
Jaime Convery <jaimeconvery@gmail.com>
2022-03-23 19:42:45 -0400

    feat: my seats component when zoomed in on map

---
+++ frontend/src/components/PurchaseTickets/TicketPurchase/SeatSelection/MySeats/MySeats.js
@@ -0,0 +1,364 @@
+import React from 'react';
+
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+import './mySeats.scss';
+
+export default function MySeats() {
+	return (
+		<ListGroup as="ul" id="seats">
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+			<ListGroup.Item as="li" className="seat-card">
+				<Stack direction="horizontal" className="seat-info">
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Section</span>
+						<span className="seat-info-block-value">101</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Row</span>
+						<span className="seat-info-block-value">5</span>
+					</div>
+					<div className="seat-info-block">
+						<span className="seat-info-block-label">Seat</span>
+						<span className="seat-info-block-value">9</span>
+					</div>
+				</Stack>
+				<Stack direction="horizontal" className="justify-content-between pt-2">
+					<div>
+						<span className="caption">General Addmissions</span>
+					</div>
+					<div>
+						<span className="fw-bold text-end">$30.00</span>
+					</div>
+				</Stack>
+				<Button variant="outline-light" className="btn--icon-lg" />
+			</ListGroup.Item>
+		</ListGroup>
+	);
+}
