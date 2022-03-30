e21d20c8b5bda97d04f51c101365c42788d0c8f5
Jaime Convery <jaimeconvery@gmail.com>
2022-03-30 17:19:03 -0400

    fix: venue page - no scrollable

--- frontend/src/components/VenueInformation/VenueInformation.js
+++ frontend/src/components/VenueInformation/VenueInformation.js
@@ -11,6 +11,10 @@ import { VenueList } from './VenueList';
 import './venueInformation.scss';
 
 export default function VenueInformation() {
+	const [
+		key,
+		setKey
+	] = useState('upcoming');
 	return (
 		<div id="venue">
 			<Row className="gap-3">
@@ -18,10 +22,17 @@ export default function VenueInformation() {
 					<VenueCard />
 				</Col>
 				<Col md={6} xxl={6} className="flex-grow-1">
-					<h1 className="fs-md scrollable-heading">Upcoming Events</h1>
-					<div className="scrollable-container">
-						<VenueList />
-					</div>
+					<Tabs
+						id="controlled-tab"
+						defaultActiveKey="upcoming"
+						variant="pills"
+						activeKey={key}
+						onSelect={(k) => setKey(k)}
+						className="mb-4">
+						<Tab eventKey="upcoming" title="Upcoming Events">
+							<VenueList />
+						</Tab>
+					</Tabs>
 				</Col>
 				<ul />
 			</Row>
