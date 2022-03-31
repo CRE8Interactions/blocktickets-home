7c583254838f48ce9d3bda7eeaf084bffaf5ce81
Jaime Convery <jaimeconvery@gmail.com>
2022-03-31 17:38:27 -0400

    refactor: fix svg icon and change icon button props

--- frontend/src/components/IconButton/IconButton.js
+++ frontend/src/components/IconButton/IconButton.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 import './iconButton.scss';
 
-export default function IconButton({ children, styles, variant, btn, background, size, link }) {
+export default function IconButton({ children, styles, variant, btn, size, link }) {
 	return (
 		<Link
 			to={`${link}`}
 			className={`btn btn-${variant} d-flex btn-${btn} icon-button ${styles && styles} ${size
 				? `btn-${size}`
-				: ''} ${background && `btn-${background} `} `}>
+				: ''} `}>
 			{children}
 		</Link>
 	);
