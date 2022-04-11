57fca764c20334f6b2088a60a760c75f32a30da4
Jaime Convery <jaimeconvery@gmail.com>
2022-04-11 11:24:51 -0400

    style: additional styling

--- frontend/src/components/TicketCard/TicketModal/TransferModal/TransferModal.js
+++ frontend/src/components/TicketCard/TicketModal/TransferModal/TransferModal.js
@@ -1,10 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+import { Error } from './../../../Error';
+
+import './transferModal.scss';
+
 export default function TransferModal() {
+	const [
+		status,
+		setStatus
+	] = useState('transfer');
+
 	return (
 		<Fragment>
 			<Modal.Header closeButton>
@@ -14,22 +23,46 @@ export default function TransferModal() {
 				</div>
 			</Modal.Header>
 			<Modal.Body>
-				<div className="modal-heading">
-					<h4 className="modal-heading-title">Enter the recepient phone number </h4>
-					<p className="small text-muted">
-						The recipient will get notified via text that you have transferred your
-						ticket to them.
-					</p>
-				</div>
-				<Form>
-					<Form.Group controlId="phone-number">
-						<Form.Label>Phone Number</Form.Label>
-						<Form.Control type="text" placeholder="(416) 232 3423" />
-						<Button variant="primary" size="lg" type="submit">
-							Transfer
-						</Button>
-					</Form.Group>
-				</Form>
+				{status === 'successful' ? (
+					<Fragment>
+						<div
+							id="success"
+							className="modal-heading d-flex-column justify-content-center align-items-center gap-4">
+							<h4 className="modal-heading-title">Transfer completed!</h4>
+						</div>
+						<p className="small text-muted">
+							We have transferred your ticket to the recipient. If they have not
+							received it please reach out to us.
+						</p>
+					</Fragment>
+				) : (
+					<Fragment>
+						<div className="modal-heading">
+							<h4 className="modal-heading-title">
+								Enter the recepient phone number{' '}
+							</h4>
+							<p className="small text-muted">
+								The recipient will get notified via text that you have transferred
+								your ticket to them.
+							</p>
+						</div>
+						<Form>
+							<Form.Group controlId="phone-number">
+								<Form.Label>Phone Number</Form.Label>
+								<Form.Control type="tel" placeholder="(416) 232 3423" />
+							</Form.Group>
+							<Form.Control.Feedback type="invalid">
+								<Error />
+							</Form.Control.Feedback>
+							<Button
+								onClick={() => setStatus('successful')}
+								variant="primary"
+								size="lg">
+								Transfer
+							</Button>
+						</Form>
+					</Fragment>
+				)}
 			</Modal.Body>
 		</Fragment>
 	);
