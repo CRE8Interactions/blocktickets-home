abf67770314425248950864fb0549b8d1b02b0a2
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-04-13 11:25:22 -0400

    Use CA for prod DB

--- tickets/config/env/preview/database.js
+++ tickets/config/env/preview/database.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        ca: fs.readFileSync(`${__dirname}/ca-certificate.crt`).toString(),
       },
     },
     options: {
