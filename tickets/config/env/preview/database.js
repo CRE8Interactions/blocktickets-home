9d23f34e418ec479bd19903efe4af5d0aaa07831
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-04-13 10:49:43 -0400

    DB SSL update 2

--- tickets/config/env/preview/database.js
+++ tickets/config/env/preview/database.js
@@ -9,8 +9,12 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: env('DATABASE_SSL', false)
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL', false),
+      },
     },
-    options: {}
+    options: {
+      ssl: env('DATABASE_SSL', false)
+    }
   },
 });
