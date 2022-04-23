430f001ef442b422372a1899b26c77595632cb31
Jaime Convery <jaimeconvery@gmail.com>
2022-04-23 17:21:30 -0400

    refactior: change markup tag

--- frontend/src/pages/SettingsPage/LoginSecurityPage/LoginSecurityPage.js
+++ frontend/src/pages/SettingsPage/LoginSecurityPage/LoginSecurityPage.js
@@ -7,10 +7,10 @@ export default function LoginSecurityPage() {
 		<SettingsWrapper>
 			<div className="heading">
 				<h1 className="fs-md">Login Security</h1>
-				<p className="normal text-muted fw-normal m-0">
+				<h2 className="subtitle">
 					Update the phone number associated with this account, please fill in the
 					following fields. Your phone number is requested for security reasons.
-				</p>
+				</h2>
 				<p className="mt-3 fw-medium">
 					Current Phone Number: <span className="text-secondary" />
 				</p>
