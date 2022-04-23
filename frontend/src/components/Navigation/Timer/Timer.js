9ef561365d2272ef5117b07fc1c9a9449f385151
Jaime Convery <jaimeconvery@gmail.com>
2022-04-22 20:46:52 -0400

    refactor: moved the timer check and popups to timer component from Checkout

--- frontend/src/components/Navigation/Timer/Timer.js
+++ frontend/src/components/Navigation/Timer/Timer.js
@@ -3,22 +3,47 @@ import * as moment from 'moment';
 
 import { COUNTDOWN_NUM, COUNTDOWN_DURATION } from './../../../utilities/helpers';
 
+import { CheckoutModal } from '../../CheckoutWrapper/Checkout/CheckoutModal';
+
 import './timer.scss';
 
 export default function Timer() {
+
 	const [
 		remaining,
 		setRemaining
 	] = useState();
 
-	const currentTime = moment().unix();
+	const [
+		remainingMinutes,
+		setRemainingMinutes
+	] = useState();
+
+	const [
+		remainingSeconds,
+		setRemainingSeconds
+	] = useState();
+
+	// modal state
+		const [
+		show,
+		setShow
+	] = useState(false);
+
+	const [
+		modalType,
+		setModalType
+	] = useState();
+
+const currentTime = moment().unix();
 	const endTime = moment().add(COUNTDOWN_NUM, 'minutes').unix();
 	const diffTime = endTime - currentTime;
 	let duration = moment.duration(diffTime * COUNTDOWN_DURATION, 'milliseconds');
 	const interval = COUNTDOWN_DURATION;
 
+	let time; 
 	useEffect(() => {
-		let time = setInterval(() => {
+		time = setInterval(() => {
 			duration = moment.duration(duration - interval, 'milliseconds');
 			let seconds = duration.seconds();
 			if (seconds < 10) {
@@ -26,10 +51,34 @@ export default function Timer() {
 			}
 
 			setRemaining(`${duration.minutes()}:${seconds}`);
+			setRemainingMinutes(duration.minutes())
+			setRemainingSeconds(seconds)
 		}, interval);
 	}, []);
 
+	const handleShow = () => setShow(true);
+
+	 useEffect(() => {
+
+	// check if timer is halfway every time minute changes 
+		if (remainingMinutes == COUNTDOWN_NUM / 2) {
+			handleShow();
+			setModalType('reminder')
+		}
+	}, [ remainingMinutes])
+
+	// check if timer is finished every time seconds change  
+	useEffect(() => {
+	 	  if (remainingMinutes === 0 && remainingSeconds == '00') {
+			handleShow();
+			setModalType('timeout')
+			clearInterval(time);
+		}
+	}, [remainingSeconds])
+
+
 	return (
+		<>
 		<div id="timer-container">
 			<div id="timer">
 				{remaining &&
@@ -41,5 +90,12 @@ export default function Timer() {
 				{remaining && remaining.includes('-') && <span>Session expired</span>}
 			</div>
 		</div>
+
+		<CheckoutModal
+				modalType={modalType}
+				setModalType={setModalType}
+				show={show}
+				setShow={setShow} />
+		</>
 	);
 }
