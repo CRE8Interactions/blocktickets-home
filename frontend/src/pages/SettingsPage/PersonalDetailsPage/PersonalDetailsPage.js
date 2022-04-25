040105b1f5c237393187f456a8a3837b259d14a3
Jaime Convery <jaimeconvery@gmail.com>
2022-04-25 14:23:21 -0400

    refactor: rearrange components

--- frontend/src/pages/SettingsPage/PersonalDetailsPage/PersonalDetailsPage.js
+++ frontend/src/pages/SettingsPage/PersonalDetailsPage/PersonalDetailsPage.js
@@ -6,7 +6,7 @@ export default function PersonalDetailsPage() {
 	return (
 		<SettingsWrapper>
 			<div className="heading">
-				<h1 className="fs-md">Personal Details</h1>
+				<h1 className="page-title">Personal Details</h1>
 				<h2 className="subtitle">Access and change your personal details</h2>
 			</div>
 			<PersonalDetailsForm />
