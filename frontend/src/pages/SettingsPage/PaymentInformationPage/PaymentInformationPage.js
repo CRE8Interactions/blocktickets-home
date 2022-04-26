30db65018bf86d6b0256a5acfb569d065aae3869
Jaime Convery <jaimeconvery@gmail.com>
2022-04-26 14:54:32 -0400

    style: styling changes

--- frontend/src/pages/SettingsPage/PaymentInformationPage/PaymentInformationPage.js
+++ frontend/src/pages/SettingsPage/PaymentInformationPage/PaymentInformationPage.js
@@ -5,9 +5,9 @@ import { SettingsWrapper, BankAccount } from '../../../components';
 export default function PaymentInformationPage() {
 	return (
 		<SettingsWrapper>
-			<div className="heading">
-				<h1 className="page-title">Payment Information</h1>
-				<h2 className="subtitle">
+			<div className="settings-heading">
+				<h1 className="settings-title">Payment Information</h1>
+				<h2 className="settings-subtitle">
 					The money you make from selling your tickets will be issued to the bank account
 					associated with this account
 				</h2>
--- frontend/src/pages/SettingsPage/PersonalDetailsPage/PersonalDetailsPage.js
+++ frontend/src/pages/SettingsPage/PersonalDetailsPage/PersonalDetailsPage.js
@@ -5,9 +5,9 @@ import { SettingsWrapper, PersonalDetailsForm } from '../../../components';
 export default function PersonalDetailsPage() {
 	return (
 		<SettingsWrapper>
-			<div className="heading">
-				<h1 className="page-title">Personal Details</h1>
-				<h2 className="subtitle">Access and change your personal details</h2>
+			<div className="settings-heading">
+				<h1 className="settings-title">Personal Details</h1>
+				<h2 className="settings-subtitle">Access and change your personal details</h2>
 			</div>
 			<PersonalDetailsForm />
 		</SettingsWrapper>
