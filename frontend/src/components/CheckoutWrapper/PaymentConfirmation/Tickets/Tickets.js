c2575b37d8fb730c439471facd5dfa708ee6ca8f
Jaime Convery <jaimeconvery@gmail.com>
2022-04-26 17:30:10 -0400

    style: card sizing and style changes

--- frontend/src/components/CheckoutWrapper/PaymentConfirmation/Tickets/Tickets.js
+++ frontend/src/components/CheckoutWrapper/PaymentConfirmation/Tickets/Tickets.js
@@ -1,29 +1,30 @@
 import React, { Fragment } from 'react';
-import { Ticket } from './Ticket';
+import { Link } from 'react-router-dom';
 
 import Stack from 'react-bootstrap/Stack';
-import Button from 'react-bootstrap/Button';
+
+import { Ticket } from './Ticket';
 
 import './tickets.scss';
 
-export default function Tickets({order}) {
-
+export default function Tickets({ order }) {
 	return (
 		<Fragment>
 			<h1 className="section-title section-title--muted">Tickets</h1>
 
 			<Stack gap={4} as="ul" className="mt-md-3 ">
-				{ order && order.tickets.map((ticket, index) => {
-					return (
-						<li className="ticket" key={index}>
-							<Ticket ticket={ticket} order={order}/>
-						</li>
-					)
-				})}
+				{order &&
+					order.tickets.map((ticket, index) => {
+						return (
+							<li className="ticket" key={index}>
+								<Ticket ticket={ticket} order={order} />
+							</li>
+						);
+					})}
 			</Stack>
-			<Button variant="primary" id="myWalletBtn">
-				Go to My Wallet
-			</Button>
+			<Link to="upcoming-events" className="btn btn-primary" id="myWalletBtn">
+				Go to My Events
+			</Link>
 		</Fragment>
 	);
 }
