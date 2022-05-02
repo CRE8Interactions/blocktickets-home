7cc2d813d365e7654f9a74aa49f6113736f4a0b9
Jaime Convery <jaimeconvery@gmail.com>
2022-05-02 14:04:16 -0400

    fix: num tickets counter btns moving

--- frontend/src/components/PurchaseTickets/TicketPurchase/TicketConfirmation/TicketConfirmation.js
+++ frontend/src/components/PurchaseTickets/TicketPurchase/TicketConfirmation/TicketConfirmation.js
@@ -4,9 +4,6 @@ import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
 
-import plus from '../../../../assets/icons/plus.svg';
-import minus from '../../../../assets/icons/minus.svg';
-
 import { TicketPurchaseFooter } from '../TicketPurchaseFooter';
 import { BackButton } from '../../../BackButton';
 
@@ -79,15 +76,20 @@ export default function TicketConfirmation({ handleGoBack, type, ticket }) {
 							<span className="caption fw-normal text-muted">${ticket.attributes.cost.toFixed(2)} ea + Fees</span>
 						</p>
 					</div>
-					<Stack direction="horizontal" gap={3} className="counter fw-bolder">
+					<Stack direction="horizontal" className="counter fw-bolder">
 						<Button title="decrease quantity of tickets" className="btn--icon" variant="outline-light" onClick={() => setTicketCount(ticketCount - 1)} disabled={ticketCount === ticket.attributes.minimum_quantity}
 						aria-disabled={ticketCount === ticket.attributes.minimum_quantity} >
-							<img src={minus} alt="" />
+							<svg role="img" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fillRule="evenodd" clipRule="evenodd" d="M6.875 12C6.875 11.4477 7.32272 11 7.875 11H17.875C18.4273 11 18.875 11.4477 18.875 12C18.875 12.5523 18.4273 13 17.875 13H7.875C7.32272 13 6.875 12.5523 6.875 12Z" fill="#777E91"/>
+</svg>
 						</Button> 
-						<span className='flex-grow-1'>{ ticketCount }</span>
+						<span className='flex-grow-1 text-center'>{ ticketCount }</span>
 						<Button title="increase quantity of tickets" className="btn--icon ms-0" variant="outline-light" onClick={() => setTicketCount(ticketCount + 1)} disabled={ticketCount >= ticket.attributes.maximum_quantity}
 						aria-disabled={ticketCount >= ticket.attributes.maximum_quantity} >
-							<img src={plus} alt="" />
+							<svg role="img" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fillRule="evenodd" clipRule="evenodd" d="M13.95 7C13.95 6.44772 13.5022 6 12.95 6C12.3977 6 11.95 6.44772 11.95 7V11H7.94995C7.39767 11 6.94995 11.4477 6.94995 12C6.94995 12.5523 7.39767 13 7.94995 13H11.95V17C11.95 17.5523 12.3977 18 12.95 18C13.5022 18 13.95 17.5523 13.95 17V13H17.95C18.5022 13 18.95 12.5523 18.95 12C18.95 11.4477 18.5022 11 17.95 11H13.95V7Z" fill="#777E91"/>
+</svg>
+
 						</Button>
 					</Stack>
 				</Stack>
