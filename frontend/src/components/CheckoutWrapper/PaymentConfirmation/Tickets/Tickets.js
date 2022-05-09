134312312729c15f98f509261c40507a8b43187c
Jaime Convery <jaimeconvery@gmail.com>
2022-05-09 18:51:08 -0400

    refactor: change link button

--- frontend/src/components/CheckoutWrapper/PaymentConfirmation/Tickets/Tickets.js
+++ frontend/src/components/CheckoutWrapper/PaymentConfirmation/Tickets/Tickets.js
@@ -22,7 +22,7 @@ export default function Tickets({ order }) {
 						);
 					})}
 			</Stack>
-			<Link to="/upcoming-events" className="btn btn-primary" id="myWalletBtn">
+			<Link to="/my-events" className="btn btn-primary" id="myEventsBtn">
 				Go to My Events
 			</Link>
 		</Fragment>
