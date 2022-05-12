e68889aa4169c405f9a8685a8577ea8273e2387f
Jaime Convery <jaimeconvery@gmail.com>
2022-05-12 14:04:58 -0400

    refactor: remove unused code

--- frontend/src/components/EventDetailsWrapper/ActionBtns/ActionBtns.js
+++ frontend/src/components/EventDetailsWrapper/ActionBtns/ActionBtns.js
@@ -3,15 +3,12 @@ import React from 'react';
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
 
-import './actionBtns.scss';
-
 export default function ActionBtns({ handleClick, ticketStatus }) {
 	return (
 		<Stack
 			direction="horizontal"
 			gap={3}
-			id="action-btns"
-			className="btn-group-flex justify-content-center justify-content-md-end mt-md-2">
+			className="btn-group-flex justify-content-center action-btns justify-content-md-end mt-md-2">
 			<Button
 				onClick={() => handleClick('transfer')}
 				variant="dark"
