2d427ad5f0f39b962dbeec19a96c277171f20542
Jaime Convery <jaimeconvery@gmail.com>
2022-05-16 18:42:05 -0400

    refactor: move code around

--- frontend/src/pages/EventDetailsPage/EventDetailsPage.js
+++ frontend/src/pages/EventDetailsPage/EventDetailsPage.js
@@ -4,13 +4,21 @@ import { useParams } from 'react-router-dom';
 import { EventDetailsWrapper } from '../../components';
 
 export default function EventDetailsPage() {
-	let { id } = useParams();
+	let { orderId } = useParams();
+
+	useEffect(
+		() => {
+			getOrder(orderId).then((res) => setOrder(res.data)).catch((err) => console.error(err));
+		},
+		[
+			orderId
+		]
+	);
+
 	const [
 		order,
 		setOrder
-	] = useState('');
-	// useEffect(() => {
-	// 	getOrder(id).then((res) => setOrder(res.data)).catch((err) => console.error(err));
-	// }, []);
-	return <EventDetailsWrapper id={id} />;
+	] = useState();
+
+	return <EventDetailsWrapper order={order} />;
 }
