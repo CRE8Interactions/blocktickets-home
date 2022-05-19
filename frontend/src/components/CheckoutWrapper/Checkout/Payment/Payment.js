064dc6beec35d3da7d01298603770c0ec206aecc
Jaime Convery <jaimeconvery@gmail.com>
2022-05-18 20:27:18 -0400

    fix: checkout modals

--- frontend/src/components/CheckoutWrapper/Checkout/Payment/Payment.js
+++ frontend/src/components/CheckoutWrapper/Checkout/Payment/Payment.js
@@ -1,18 +1,14 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import Form from 'react-bootstrap/Form';
 import { PaymentElement } from '@stripe/react-stripe-js';
 
 import './payment.scss';
 
-export default function Payment({checkValid}) {	
+export default function Payment({ checkValid }) {
 	let name;
 	const CheckoutForm = () => {
 		return (
 			<Form id="checkout-form">
-				<Form.Group className="form-group" controlId="card-holder">
-					<Form.Label>Name on Card</Form.Label>
-					<Form.Control type="text" name="card-holder" onChange={ (e) => name = e.target.value } autoComplete={'off'} />
-				</Form.Group>
 				<PaymentElement onChange={(e) => checkValid(e, name)} />
 			</Form>
 		);
@@ -22,7 +18,7 @@ export default function Payment({checkValid}) {
 		<Fragment>
 			<h1 className="section-title mb-3">Payment</h1>
 			<h2 className="text-muted small fw-medium">Please select a payment method</h2>
-				<CheckoutForm />
+			<CheckoutForm />
 		</Fragment>
 	);
 }
