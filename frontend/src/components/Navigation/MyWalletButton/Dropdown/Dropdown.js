7800d409f07cf18d1cf1c199b947a53dfc374d46
Jaime Convery <jaimeconvery@gmail.com>
2022-05-25 15:22:03 -0400

    fix: opening/closing of wallet dropdown

--- frontend/src/components/Navigation/MyWalletButton/Dropdown/Dropdown.js
+++ frontend/src/components/Navigation/MyWalletButton/Dropdown/Dropdown.js
@@ -4,9 +4,9 @@ import { MyWallet } from '../../MyWallet';
 
 import './dropdown.scss';
 
-export default function Dropdown({ reference, handleClick }) {
+export default function Dropdown({ handleClick }) {
 	return (
-		<div className="myDropdown" ref={reference}>
+		<div className="myDropdown">
 			<MyWallet handleClick={handleClick} />
 		</div>
 	);
