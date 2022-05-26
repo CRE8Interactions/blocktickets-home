85d42be48bf0e80659b90b625c96010168bf2f89
Jaime Convery <jaimeconvery@gmail.com>
2022-05-26 18:14:05 -0400

    fix: transfer card sizing

--- frontend/src/components/TransferCard/TransferCard.js
+++ frontend/src/components/TransferCard/TransferCard.js
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 export default function TransferCard({transfer, cancel, status, acceptTransfer}) {
   return (
     <Fragment>
-			<Card body className="ticket-card card-md">
+			<Card body className="ticket-card">
       <Card.Img
 					src={transfer?.event?.image?.url}
 					width="217"
