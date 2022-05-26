c5ddd0cc98a00cae441113e6cc70e1c8332d3aa4
Jaime Convery <jaimeconvery@gmail.com>
2022-05-26 18:24:29 -0400

    fix: new footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,88 +1,116 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
+
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
-import logo from '../../assets/logo.svg';
 import { getCopyrightYear } from '../../utilities/helpers';
 import InputField from './../InputField/InputField';
+
+import logo from '../../assets/logo.svg';
+import facebook from '../../assets/icons/facebook-filled.svg';
+import instagram from '../../assets/icons/instagram.svg';
+import twitter from '../../assets/icons/twitter-filled.svg';
+import tiktok from '../../assets/icons/tiktok-filled.svg';
+import discord from '../../assets/icons/discord-filled.svg';
+
 import './footer.scss';
 
 export default function Footer() {
 	return (
 		<footer id="footer">
-			<Row className="footer-spacer main-content d-flex flex-column gap-lg-3 flex-md-row">
-				<Col>
+			<Row className="footer-spacer main-content d-flex  gap-lg-3 flex-column flex-lg-row">
+				<Col lg={4} className="col">
 					<Link to={'/'} className="brand-logo">
 						<img src={logo} alt="blocktickets" />
 					</Link>
 					<p className="tag-line">The future of ticketing is here</p>
 				</Col>
-				<Col md={2} className="col">
-					<h3 className="footer-nav-list-title caption--uppercase">Blocktickets</h3>
-					<ul>
-						<li>
-							<LinkContainer to="/">
-								<Nav.Link to="">cta 1</Nav.Link>
-							</LinkContainer>
-						</li>
-						<li>
-							<LinkContainer to="/">
-								<Nav.Link to="">cta 1</Nav.Link>
-							</LinkContainer>
-						</li>
-						<li>
-							<LinkContainer to="/">
-								<Nav.Link to="">cta 1</Nav.Link>
-							</LinkContainer>
-						</li>
-					</ul>
+				<Col xs={10} lg="auto" xl={5} className="col">
+					<Stack direction="horizontal" className="main-links">
+						<ul>
+							<li>
+								<LinkContainer to="/">
+									<Nav.Link to="">Terms & Conditions</Nav.Link>
+								</LinkContainer>
+							</li>
+							<li>
+								<LinkContainer to="/">
+									<Nav.Link to="">Purchase Policy</Nav.Link>
+								</LinkContainer>
+							</li>
+							<li>
+								<LinkContainer to="/">
+									<Nav.Link to="">Privacy Policy</Nav.Link>
+								</LinkContainer>
+							</li>
+							<li>
+								<LinkContainer to="/">
+									<Nav.Link to="">Cookies Policy</Nav.Link>
+								</LinkContainer>
+							</li>
+						</ul>
+						<ul>
+							<li>
+								<LinkContainer to="/">
+									<Nav.Link to="">Disclaimer</Nav.Link>
+								</LinkContainer>
+							</li>
+							<li>
+								<LinkContainer to="/login">
+									<Nav.Link to="">Client Login</Nav.Link>
+								</LinkContainer>
+							</li>
+							<li>
+								<LinkContainer to="/">
+									<Nav.Link to="">About Us</Nav.Link>
+								</LinkContainer>
+							</li>
+							<li>
+								<LinkContainer to="/">
+									<Nav.Link to="">Help</Nav.Link>
+								</LinkContainer>
+							</li>
+						</ul>
+					</Stack>
 				</Col>
-				<Col md={2} className="col">
-					<h3 className="footer-nav-list-title caption--uppercase">Info</h3>
-					<ul>
+				<Col className="col">
+					<h3 className="footer-title">Also find us on</h3>
+					<Stack as="ul" direction="horizontal" className="justify-content-between">
 						<li>
-							<LinkContainer to="/">
-								<Nav.Link to="">cta 1</Nav.Link>
-							</LinkContainer>
+							<a href="">
+								<img src={facebook} alt="facebook" />
+							</a>
 						</li>
 						<li>
-							<LinkContainer to="/">
-								<Nav.Link to="">cta 1</Nav.Link>
-							</LinkContainer>
+							<a href="">
+								<img src={instagram} alt="instagram" />
+							</a>
 						</li>
 						<li>
-							<LinkContainer to="/">
-								<Nav.Link to="">cta 1</Nav.Link>
-							</LinkContainer>
+							<a href="">
+								<img src={twitter} alt="twitter" />
+							</a>
 						</li>
-					</ul>
-				</Col>
-				<Col>
-					<h3 className="footer-title">Join Newsletter</h3>
-					<p className="small">
-						Subscribe to our newsletter to get notified over the latest releases
-					</p>
-					<InputField
-						type="email"
-						placeholder="Enter your email"
-						color="primary"
-						size="lg"
-						styles="mt-4"
-					/>
+						<li>
+							<a href="">
+								<img src={tiktok} alt="tiktok" />
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<img src={discord} alt="discord" />
+							</a>
+						</li>
+					</Stack>
 				</Col>
 			</Row>
 
 			<div className="d-flex flex-column flex-md-row justify-content-md-between align-items-center py-4 copyright caption">
-				<p className="text-muted">
-					Copyright &copy; {getCopyrightYear()} Blocktickets. All rights reserved
-				</p>
-				<p>
-					We use cookies for better service. <Button variant="link">Accept</Button>
-				</p>
+				<p className="text-muted">Copyright &copy; {getCopyrightYear()} Blocktickets. All rights reserved</p>
 			</div>
 		</footer>
 	);
