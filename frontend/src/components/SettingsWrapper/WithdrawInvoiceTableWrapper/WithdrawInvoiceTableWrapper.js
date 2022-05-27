a7229915d776809b420131eb932e50a866adc76c
Jaime Convery <jaimeconvery@gmail.com>
2022-05-27 14:38:08 -0400

    refactor: add totalFunds to withdraw cards and format total number

--- frontend/src/components/SettingsWrapper/WithdrawInvoiceTableWrapper/WithdrawInvoiceTableWrapper.js
+++ frontend/src/components/SettingsWrapper/WithdrawInvoiceTableWrapper/WithdrawInvoiceTableWrapper.js
@@ -13,16 +13,16 @@ export default function WithdrawInvoiceTableWrapper({ details }) {
 			<section id="withdraw">
 				<div className="settings-heading">
 					<h1 className="settings-title">Withdraw</h1>
-					<h2 className="settings-subtitle">Withdraw funds from your account</h2>
+					<h2 className="settings-subtitle">Withdraw funds from your account.</h2>
 				</div>
-			<Withdraw />
+			<Withdraw details={details} />
 			</section>
 		</SettingsWrapper>
 		<section id="invoices">
 				<div id="invoices-heading-container">
 					<div className="settings-heading">
 							<h1 className="settings-title">Invoices</h1>
-							<h2 className="settings-subtitle">View / download your previous invoices and transactions below</h2>
+							<h2 className="settings-subtitle">View / download your previous invoices and transactions below.</h2>
 						</div>
 				</div>
 			<InvoiceTable details={details} />
