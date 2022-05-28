cd5c47e542b5053a2fce7779b5b64fbce7c56f0d
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-05-28 05:33:44 -0400

    show results on search page

--- frontend/src/components/SearchWrapper/SearchWrapper.js
+++ frontend/src/components/SearchWrapper/SearchWrapper.js
@@ -1,16 +1,24 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
-
 import { SearchList } from '../SearchList';
+import { searchEvents } from '../../utilities/api';
 
 export default function SearchWrapper({ query }) {
 	// api is called with limited results
+	const [results, setResults] = useState()
+	useEffect(() => {
+		let data = {
+			data: query
+		};
+		searchEvents(data)
+			.then(res => setResults(res.data))
+			.catch(err => console.error(err))
+	}, [query])
 
 	return (
 		<>
-			<h1 className="fs-md mb-5">We found 128 results for "{query}"</h1>
-			<SearchList />
+			<h1 className="fs-md mb-5">We found {results?.length} results for "{query}"</h1>
+			<SearchList queryResults={results} />
 			<Button variant="outline-light" className="mt-4">
 				Load more
 			</Button>
