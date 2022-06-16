5426c98ff7fcf91359baf446a6edafd6320099f5
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-06-16 08:31:51 -0400

    email template plugin

--- tickets/config/middlewares.js
+++ tickets/config/middlewares.js
@@ -1,6 +1,18 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', "strapi.io", "cdn.jsdelivr.net", `https://${process.env.DO_SPACE_BUCKET}.${process.env.DO_SPACE_ENDPOINT}`],
+          'media-src': ["'self'", 'data:', 'blob:', `https://${process.env.DO_SPACE_BUCKET}.${process.env.DO_SPACE_ENDPOINT}`],
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net', "editor.unlayer.com"],
+          "frame-src": ["'self'", "editor.unlayer.com", `https://${process.env.DO_SPACE_BUCKET}.${process.env.DO_SPACE_ENDPOINT}`],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
