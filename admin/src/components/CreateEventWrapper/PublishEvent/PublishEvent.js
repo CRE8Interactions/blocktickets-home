d6947b3a0147970e3ca6ce525cf2b54b0e2737ef
Jaime Convery <jaimeconvery@gmail.com>
2022-07-05 17:36:31 -0400

    refactor: html markup

--- admin/src/components/CreateEventWrapper/PublishEvent/PublishEvent.js
+++ admin/src/components/CreateEventWrapper/PublishEvent/PublishEvent.js
@@ -55,30 +55,28 @@ export default function PublishEvent() {
                 <Col>
                     <h1 className='fs-md mb-3'>Event Info</h1>
                     <ul>
-                        <li className="list">
-                            <Stack as="li" gap={3}>
-                                <Stack as="li" direction='horizontal' className='split-row'>
-                                    <span className='text-muted'>Presented by</span>
-                                    <span className='fw-medium text-end flex-grow-1'>Southside Music Hall</span>
-                                </Stack>
-                                <Stack as="li" direction='horizontal' className='split-row'>
-                                    <span className='text-muted'>Event title</span>
-                                    <span className='fw-medium text-end flex-grow-1'>Nic Fanciulli</span>
-                                </Stack>
-                                <Stack as="li" direction='horizontal' className='split-row'>
-                                    <span className='text-muted'>Date</span>
-                                    <span className='fw-medium text-end flex-grow-1'>May 2, 2022</span>
-                                </Stack>
-                                <Stack as="li" direction='horizontal' className='split-row'>
-                                    <span className='text-muted'>Venue</span>
-                                    <span className='fw-medium text-end flex-grow-1'>Southside Music Hall</span>
-                                </Stack>
-                                <Stack as="li" direction='horizontal' className='split-row'>
-                                    <span className='text-muted'>Location</span>
-                                    <span className='fw-medium text-end flex-grow-1'>1135 Botham Jean Blvd, Dallas, TX</span>
-                                </Stack>
+                        <Stack as="li" gap={3} className="list">
+                            <Stack as="li" direction='horizontal' className='split-row'>
+                                <span className='text-muted'>Presented by</span>
+                                <span className='fw-medium text-end flex-grow-1'>Southside Music Hall</span>
                             </Stack>
-                        </li>
+                            <Stack as="li" direction='horizontal' className='split-row'>
+                                <span className='text-muted'>Event title</span>
+                                <span className='fw-medium text-end flex-grow-1'>Nic Fanciulli</span>
+                            </Stack>
+                            <Stack as="li" direction='horizontal' className='split-row'>
+                                <span className='text-muted'>Date</span>
+                                <span className='fw-medium text-end flex-grow-1'>May 2, 2022</span>
+                            </Stack>
+                            <Stack as="li" direction='horizontal' className='split-row'>
+                                <span className='text-muted'>Venue</span>
+                                <span className='fw-medium text-end flex-grow-1'>Southside Music Hall</span>
+                            </Stack>
+                            <Stack as="li" direction='horizontal' className='split-row'>
+                                <span className='text-muted'>Location</span>
+                                <span className='fw-medium text-end flex-grow-1'>1135 Botham Jean Blvd, Dallas, TX</span>
+                            </Stack>
+                        </Stack>
                         <li className="list pt-2">
                             <Stack as="ul" gap={3}>
                                 <Stack as="li" direction='horizontal' className='split-row'>
