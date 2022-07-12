1d16a17a279cd764f05a3ba9a7ccdd6e7aa9ae41
Jaime Convery <jaimeconvery@gmail.com>
2022-07-12 17:35:22 -0400

    refactor: additional styling and changes

--- admin/src/components/DeleteModal/DeleteModal.js
+++ admin/src/components/DeleteModal/DeleteModal.js
@@ -7,13 +7,17 @@ import Button from 'react-bootstrap/Button';
 import './deleteModal.scss';
 
 export default function DeleteModal({ show, handleClose }) {
+
+    const handleDelete = () => {
+        handleClose();
+    }
     return (
         <Modal id="delete-modal" centered animation={false} fullscreen="md-down" show={show} onHide={handleClose}>
             <Modal.Body>
                 <h1 className="modal-body-heading-title">Are you sure you want to delete this ticket?</h1>
                 <p>You will not be able to restore this action</p>
                 <Stack className="btn-group-flex">
-                    <Button variant="outline-light" size="lg" className='text-danger'>
+                    <Button variant="outline-light" size="lg" className='text-danger' onClick={handleDelete}>
                         Delete ticket
                     </Button>
                     <Button variant="primary" size="lg" onClick={handleClose}>
