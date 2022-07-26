6fecf541e3e5ee16d32e5010ee5eac342cbf4c7f
Jaime Convery <jaimeconvery@gmail.com>
2022-07-26 17:52:48 -0400

    refactor: move components and style changes

--- admin/src/components/PasswordInput/PasswordInput.js
+++ admin/src/components/PasswordInput/PasswordInput.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-import { EyeIcon } from "../EyeIcon";
-import { EyeIconSlash } from "../EyeIconSlash";
+import { EyeIcon } from "./EyeIcon";
+import { EyeIconSlash } from "./EyeIconSlash";
 
 export default function PasswordInput({ value, isValid, handlePassword, reference, placeholder = "Password" }) {
 
