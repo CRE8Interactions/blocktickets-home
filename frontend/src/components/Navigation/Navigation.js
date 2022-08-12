e42889f01d1cc36b9a467f91b5f4f210478a7e16
Jaime Convery <jaimeconvery@gmail.com>
2022-08-12 16:01:03 -0400

    refactor: clean code and add styling

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -9,72 +9,72 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Stack from 'react-bootstrap/Stack';
 
-import mobileLogo from '../../assets/logo-mobile.svg';
-import desktopLogo from '../../assets/logo.svg';
-
 import { SearchBar } from './SearchBar';
 import { MyWallet } from './MyWallet';
 import { LoginButton } from './LoginButton';
 import { MyWalletButton } from './MyWalletButton';
 import { Timer } from './Timer';
 
+import mobileLogo from '../../assets/logo-mobile.svg';
+import desktopLogo from '../../assets/logo.svg';
+
 import './navigation.scss';
 
 export default function Navigation() {
-	const windowSize = useWindowSize();
+    const windowSize = useWindowSize();
 
-	const logo = windowSize < 992 ? mobileLogo : desktopLogo;
+    const logo = windowSize < 992 ? mobileLogo : desktopLogo;
 
-	const toggleOverflow = (expanded) => {
-		if (expanded) {
-			document.body.classList.add('nav-is-open');
-		}
-		else {
-			document.body.classList.remove('nav-is-open');
-		}
-	};
+    const toggleOverflow = (expanded) => {
+        if (expanded) {
+            document.body.classList.add('nav-is-open');
+        }
+        else {
+            document.body.classList.remove('nav-is-open');
+        }
+    };
 
-	return (
-		<div className="navigation position-sticky">
-			<Navbar collapseOnSelect expand="lg" onToggle={(expanded) => toggleOverflow(expanded)}>
-				<Container>
-					<LinkContainer to="/" id="logo-link">
-						<Navbar.Brand>
-							<img src={logo} alt="blocktickets" />
-						</Navbar.Brand>
-					</LinkContainer>
-					<Stack direction="horizontal" className="desktop-btns">
-						<SearchBar />
-						<LoginButton styles="desktop-only" />
-						<MyWalletButton styles="desktop-only" />
-						<Navbar.Toggle aria-controls="responsive-navbar-nav" id="toggle" className="pe-0" />
-					</Stack>
-					<Navbar.Collapse id="responsive-navbar-nav align-items-center">
-						<Nav activeKey={window.location.pathname} className="py-lg-0" as="nav">
-							<ul
-								id="main"
-								role="main-navigation"
-								className="d-flex flex-column flex-lg-row align-items-lg-center">
-								<li>
-									<LinkContainer to="/">
-										<Nav.Link>Browse</Nav.Link>
-									</LinkContainer>
-								</li>
-							</ul>
+    return (
+        <div className="navigation position-sticky">
+            <Navbar collapseOnSelect expand="lg" onToggle={(expanded) => toggleOverflow(expanded)}>
+                <Container>
+                    <LinkContainer to="/" id="logo-link">
+                        <Navbar.Brand>
+                            <img src={logo} alt="blocktickets" />
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Stack direction="horizontal" className="desktop-btns">
+                        <SearchBar />
+                        <LoginButton styles="desktop-only" />
+                        <MyWalletButton styles="desktop-only" />
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" id="toggle" className="pe-0" />
+                    </Stack>
+                    <Navbar.Collapse id="responsive-navbar-nav align-items-center">
+                        <Nav activeKey={window.location.pathname} className="py-lg-0" as="nav">
+                            <ul
+                                id="main"
+                                role="main-navigation"
+                                className="d-flex flex-column flex-lg-row align-items-lg-center">
+                                <li>
+                                    <LinkContainer to="/">
+                                        <Nav.Link>Browse</Nav.Link>
+                                    </LinkContainer>
+                                </li>
+                            </ul>
 
-							{authService.isLoggedIn() && (
-								<ul className="mobile-tablet-only" role="wallet-navigation">
-									<li className="mt-4">
-										<MyWallet />
-									</li>
-								</ul>
-							)}
-							<LoginButton styles="mobile-tablet-only" />
-						</Nav>
-					</Navbar.Collapse>
-					<Timer className="d-block" />
-				</Container>
-			</Navbar>
-		</div>
-	);
+                            {authService.isLoggedIn() && (
+                                <ul className="mobile-tablet-only" role="wallet-navigation">
+                                    <li className="mt-4">
+                                        <MyWallet />
+                                    </li>
+                                </ul>
+                            )}
+                            <LoginButton styles="mobile-tablet-only" />
+                        </Nav>
+                    </Navbar.Collapse>
+                    <Timer className="d-block" />
+                </Container>
+            </Navbar>
+        </div>
+    );
 }
--- frontend/src/components/TicketModal/DetailsModal/DetailsModal.js
+++ frontend/src/components/TicketModal/DetailsModal/DetailsModal.js
@@ -5,69 +5,69 @@ import Modal from 'react-bootstrap/Modal';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function DetailsModal({ order }) {
-	return (
-		<Fragment>
-			<Modal.Header closeButton>
-				<Modal.Title as="h4">Details</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<div className="modal-body-heading--with-border pb-2 mb-0">
-					<h4 className="modal-body-heading-title mb-2">{order?.event?.name}</h4>
-					<p>{moment(order?.event?.start).format('ddd MMM D, YYYY h A')}</p>
-				</div>
-				<ListGroup as="ul" variant="flush">
-					<ListGroup.Item as="li" className="list">
-						<p className="heading">Entry Info</p>
-						<p>GA</p>
-					</ListGroup.Item>
-					<ListGroup.Item as="li" className="list">
-						<p className="heading">Seat Location</p>
-						<p>GA</p>
-					</ListGroup.Item>
+    return (
+        <Fragment>
+            <Modal.Header closeButton>
+                <Modal.Title as="h4">Details</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="modal-body-heading--with-border pb-2 mb-0">
+                    <h4 className="modal-body-heading-title mb-2">{order?.event?.name}</h4>
+                    <p>{moment(order?.event?.start).format('ddd MMM D, YYYY h A')}</p>
+                </div>
+                <ListGroup as="ul" variant="flush">
+                    <ListGroup.Item as="li" className="list">
+                        <p className="heading">Entry Info</p>
+                        <span>GA</span>
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li" className="list">
+                        <p className="heading">Seat Location</p>
+                        <span>GA</span>
+                    </ListGroup.Item>
 
-					<ListGroup.Item as="li" className="list">
-						<p className="heading">Ticket Info</p>
-						{ order?.event?.presentedBy &&
-							<p>{order?.event?.presentedBy}</p>
-						}
-						<p>{order?.event?.name}</p>
-						<p>Doors Open at {moment(order?.event?.doorsOpen).format('h:mm A')}</p>
-						<p>{moment(order?.event?.start).format('ddd MMM D YYYY h:mm A')}</p>
-					</ListGroup.Item>
-					{/* <ListGroup.Item as="li" className="list">
-						<p className="heading">Barcode Number</p>
-						<p>6708570159387676</p>
-					</ListGroup.Item> */}
-					<ListGroup.Item as="li" className="list">
-						<p className="heading">Location</p>
-						<p>{order?.event?.venue?.name} {order?.event?.venue?.address[0]?.city} {order?.event?.venue?.address[0]?.state} {order?.event?.venue?.address[0]?.country}</p>
-					</ListGroup.Item>
-					<ListGroup.Item as="li" className="list">
-						<p className="heading">Order Number</p>
-						<p>{order?.orderId}</p>
-					</ListGroup.Item>
-					<ListGroup.Item as="li" className="list">
-						<p className="heading">Purchase Date</p>
-						<p>{moment(order?.createdAt).format('ddd, MMM D YYYY - h:mmA')}</p>
-					</ListGroup.Item>
-					<ListGroup.Item as="li" className="list">
-						<p className="heading">Contact Address</p>
-						<p className="text-secondary">Oxa7d8…..d270</p>
-					</ListGroup.Item>
-					<ListGroup.Item as="li" className="list">
-						<p className="heading">Token ID</p>
-						<p className="text-secondary">23000121</p>
-					</ListGroup.Item>
-					<ListGroup.Item as="li" className="list">
-						<p className="heading">Token Standard</p>
-						<p className="text-secondary">ERC - 721</p>
-					</ListGroup.Item>
-					<ListGroup.Item as="li" className="list">
-						<p className="heading">Blockchain</p>
-						<p className="text-secondary">Ethereum</p>
-					</ListGroup.Item>
-				</ListGroup>
-			</Modal.Body>
-		</Fragment>
-	);
+                    <ListGroup.Item as="li" className="list">
+                        <ul>
+                            <li>
+                                <p className="heading">Ticket Info</p>
+                                <ul>
+                                    {order?.event?.presentedBy &&
+                                        <span>{order?.event?.presentedBy}</span>
+                                    }
+                                    <span>{order?.event?.name}</span>
+                                    <span>Doors Open at {moment(order?.event?.doorsOpen).format('h:mm A')}</span>
+                                    <span>{moment(order?.event?.start).format('ddd MMM D YYYY h:mm A')}</span>
+                                </ul>
+                            </li>
+                        </ul>
+                        <p className="heading">Location</p>
+                        <p>{order?.event?.venue?.name} {order?.event?.venue?.address[0]?.city} {order?.event?.venue?.address[0]?.state} {order?.event?.venue?.address[0]?.country}</p>
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li" className="list">
+                        <p className="heading">Order Number</p>
+                        <p>{order?.orderId}</p>
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li" className="list">
+                        <p className="heading">Purchase Date</p>
+                        <p>{moment(order?.createdAt).format('ddd, MMM D YYYY - h:mmA')}</p>
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li" className="list">
+                        <p className="heading">Contact Address</p>
+                        <p className="text-secondary">Oxa7d8…..d270</p>
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li" className="list">
+                        <p className="heading">Token ID</p>
+                        <p className="text-secondary">23000121</p>
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li" className="list">
+                        <p className="heading">Token Standard</p>
+                        <p className="text-secondary">ERC - 721</p>
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li" className="list">
+                        <p className="heading">Blockchain</p>
+                        <p className="text-secondary">Ethereum</p>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Modal.Body>
+        </Fragment>
+    );
 }
