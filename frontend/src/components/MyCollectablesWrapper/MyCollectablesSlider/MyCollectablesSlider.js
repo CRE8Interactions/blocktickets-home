2b223a848a3e0fc19c20102ef6507f56cc89e376
Jaime Convery <jaimeconvery@gmail.com>
2022-08-20 16:12:42 -0400

    refactor: hook up swiper navigation for my collectables page

--- frontend/src/components/MyCollectablesWrapper/MyCollectablesSlider/MyCollectablesSlider.js
+++ frontend/src/components/MyCollectablesWrapper/MyCollectablesSlider/MyCollectablesSlider.js
@@ -4,20 +4,21 @@ import { SwiperSlide } from 'swiper/react';
 import { Slider } from '../../Slider';
 import { TicketCard } from '../../TicketCard';
 
-export default function MyCollectablesSlider() {
-	// will come from database
-	const ticketType = 'collectable';
+export default function MyCollectablesSlider({ navigationNextRef, navigationPrevRef }) {
 
-	return (
-		<div className="page-slider">
-			<Slider addedModule="pagination">
-				<SwiperSlide>
-					<TicketCard ticketType={ticketType} />
-				</SwiperSlide>
-				<SwiperSlide>
-					<TicketCard ticketType={ticketType} />
-				</SwiperSlide>
-			</Slider>
-		</div>
-	);
+    // will come from database
+    const ticketType = 'collectable';
+
+    return (
+        <div className="page-slider">
+            <Slider addedModule="pagination" navigationPrevRef={navigationPrevRef} navigationNextRef={navigationNextRef}>
+                <SwiperSlide className="swiper-lazy">
+                    <TicketCard ticketType={ticketType} />
+                </SwiperSlide>
+                <SwiperSlide className="swiper-lazy">
+                    <TicketCard ticketType={ticketType} />
+                </SwiperSlide>
+            </Slider>
+        </div>
+    );
 }
