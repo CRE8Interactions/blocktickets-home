a23cb251b13df8525b5d8140fc40f5075f1e1701
Jaime Convery <jaimeconvery@gmail.com>
2022-08-22 15:42:15 -0400

    refactor: change className

--- frontend/src/components/EmptyContainer/EmptyContainer.js
+++ frontend/src/components/EmptyContainer/EmptyContainer.js
@@ -4,7 +4,7 @@ import './emptyContainer.scss';
 
 export default function EmptyContainer({ children }) {
     return (
-        <div className="no-data d-flex-column justify-content-center align-items-center">
+        <div className="empty d-flex-column justify-content-center align-items-center">
             {children}
         </div>
     );
