e6712dd5220ea38fdcb4daa04e3ce34beda556ae
Jaime Convery <jaimeconvery@gmail.com>
2022-08-23 10:12:45 -0400

    fix: button sizing

--- frontend/src/components/EventDetailsWrapper/ActionBtns/ActionBtns.js
+++ frontend/src/components/EventDetailsWrapper/ActionBtns/ActionBtns.js
@@ -3,26 +3,28 @@ import React from 'react';
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
 
+import './actionBtns.scss';
+
 export default function ActionBtns({ handleClick, ticketStatus }) {
-	return (
-		<Stack
-			direction="horizontal"
-			gap={3}
-			className="btn-group-flex justify-content-center action-btns justify-content-md-end mt-md-2">
-			<Button
-				onClick={() => handleClick('transfer')}
-				variant="dark"
-				size="lg"
-				disabled={ticketStatus === 'transferred'}>
-				Transfer
-			</Button>
-			<Button
-				disabled={ticketStatus === 'listed'}
-				onClick={() => handleClick('sell')}
-				variant="dark"
-				size="lg">
-				Sell
-			</Button>
-		</Stack>
-	);
+    return (
+        <Stack
+            direction="horizontal"
+            gap={3}
+            className="btn-group-flex justify-content-center action-btns justify-content-md-end mt-md-2">
+            <Button
+                onClick={() => handleClick('transfer')}
+                variant="dark"
+                size="lg"
+                disabled={ticketStatus === 'transferred'}>
+                Transfer
+            </Button>
+            <Button
+                disabled={ticketStatus === 'listed'}
+                onClick={() => handleClick('sell')}
+                variant="dark"
+                size="lg">
+                Sell
+            </Button>
+        </Stack>
+    );
 }
