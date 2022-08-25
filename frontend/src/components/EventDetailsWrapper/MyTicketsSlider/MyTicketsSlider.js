e7fd65309c9f89cb7381a866222fb024baacac58
Jaime Convery <jaimeconvery@gmail.com>
2022-08-25 14:41:02 -0400

    fix: my tickets slider styling

--- frontend/src/components/EventDetailsWrapper/MyTicketsSlider/MyTicketsSlider.js
+++ frontend/src/components/EventDetailsWrapper/MyTicketsSlider/MyTicketsSlider.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { SwiperSlide } from 'swiper/react';
 
 import { Slider } from '../../Slider';
-import { Tickets } from '../Tickets';
+import { Ticket } from './Ticket';
 
-export default function MyTicketsSlider({ order, id, handleClick }) {
+export default function MyTicketsSlider({ order, handleClick }) {
     return (
         <div className="page-slider">
             <Slider addedModule="pagination">
                 {order && order.tickets.map((ticket, index) => {
                     return (
                         <SwiperSlide className='swiper-lazy' key={index}>
-                            <Tickets ticket={ticket} key={index} order={order} handleClick={handleClick} />
+                            <Ticket ticket={ticket} key={index} order={order} handleClick={handleClick} />
                         </SwiperSlide>
                     )
                 })
