6a33594967af458cd8047645ac76222fcb87a8f5
Jaime Convery <jaimeconvery@gmail.com>
2022-08-30 15:02:30 -0400

    refactor: clean up code

--- frontend/src/components/PurchaseTickets/TicketPurchase/TicketSelection/Ticket/Ticket.js
+++ frontend/src/components/PurchaseTickets/TicketPurchase/TicketSelection/Ticket/Ticket.js
@@ -1,77 +1,78 @@
 import React, { useEffect } from 'react';
 
-import ListGroup from 'react-bootstrap/ListGroup';
 import { ticketPrices } from '../../../../../utilities/helpers';
 
+import ListGroup from 'react-bootstrap/ListGroup';
+
 export default function Ticket({ ticket, handleNext, ticketFilters, listing }) {
-	let ticketPrice;
-	let ticketPriceWithFees;
-	let ticketName;
-	let ticketType;
-	let ticketFee;
+    let ticketPrice;
+    let ticketPriceWithFees;
+    let ticketName;
+    let ticketType;
+    let ticketFee;
 
-	useEffect(() => {
-		// Some actions
-	}, [ticket])
+    useEffect(() => {
+        // Some actions
+    }, [ticket])
 
-	const ticketTypes = (ticket) => {
-		if (!ticket?.resale && ticket?.on_sale_status === 'available') return 'Standard Ticket';
-		if (!ticket?.resale && ticket?.on_sale_status === 'presale') return 'Presale';
-		if (ticket?.resale && ticket?.on_sale_status === 'resaleAvailable') return 'Resale Ticket';
-	};
-	
-	if (ticket) {
-		let prices = ticketPrices(ticket, listing);
-		ticketPrice = `${prices.ticketCost} ea`;
-		ticketName = prices.ticketName;
-		ticketType = prices.ticketType;
-		ticketFee = prices.totalFees;
-		ticketPriceWithFees = prices.ticketCostWithFees;
-	}
+    const ticketTypes = (ticket) => {
+        if (!ticket?.resale && ticket?.on_sale_status === 'available') return 'Standard Ticket';
+        if (!ticket?.resale && ticket?.on_sale_status === 'presale') return 'Presale';
+        if (ticket?.resale && ticket?.on_sale_status === 'resaleAvailable') return 'Resale Ticket';
+    };
 
-	if (listing) {
-		// let prices = ticketPrices(ticket, listing);
-		// ticketPrice = `${prices.ticketCost} ea`;
-		// ticketName = prices.ticketName;
-		// ticketType = `Resale ${listing.tickets.length} Tickets`;
-		// ticketFee = prices.totalFees;
-		// ticketPriceWithFees = prices.ticketCostWithFees;
-	}
+    if (ticket) {
+        let prices = ticketPrices(ticket, listing);
+        ticketPrice = `${prices.ticketCost} ea`;
+        ticketName = prices.ticketName;
+        ticketType = prices.ticketType;
+        ticketFee = prices.totalFees;
+        ticketPriceWithFees = prices.ticketCostWithFees;
+    }
 
-	return (
-		<ListGroup.Item
-			onClick={() => handleNext(ticket ? ticket : null, listing ? listing : null)}
-			action
-			as="li"
-			className="d-flex justify-content-between align-items-center">
-			<div>
-				<div>
-					<span className="fw-bold p-0">{ticketName}</span>
-				</div>
-				<div>
-					<span className="text-muted caption">{ticketType}</span>
-				</div>
-			</div>
-			<div className="text-end">
-				<div>
-					<span className="fw-bold text-end">
-						{ticketFilters.showFees ? (
-							`$${ticketPriceWithFees} ea`
-						) : (
-							`$${ticketPrice}`
-						)}
-					</span>
-				</div>
-				<div>
-					{ticketFilters.showFees && (
-						<span className="text-muted caption">
-							${parseFloat(ticketPrice).toFixed(2)} + ${parseFloat(
-								ticketFee
-							).toFixed(2)} Fees
-						</span>
-					)}
-				</div>
-			</div>
-		</ListGroup.Item>
-	);
+    if (listing) {
+        // let prices = ticketPrices(ticket, listing);
+        // ticketPrice = `${prices.ticketCost} ea`;
+        // ticketName = prices.ticketName;
+        // ticketType = `Resale ${listing.tickets.length} Tickets`;
+        // ticketFee = prices.totalFees;
+        // ticketPriceWithFees = prices.ticketCostWithFees;
+    }
+
+    return (
+        <ListGroup.Item
+            onClick={() => handleNext(ticket ? ticket : null, listing ? listing : null)}
+            action
+            as="li"
+            className="d-flex justify-content-between align-items-center">
+            <div>
+                <div>
+                    <span className="fw-bold p-0">{ticketName}</span>
+                </div>
+                <div>
+                    <span className="text-muted caption">{ticketType}</span>
+                </div>
+            </div>
+            <div className="text-end">
+                <div>
+                    <span className="fw-bold text-end">
+                        {ticketFilters.showFees ? (
+                            `$${ticketPriceWithFees} ea`
+                        ) : (
+                            `$${ticketPrice}`
+                        )}
+                    </span>
+                </div>
+                <div>
+                    {ticketFilters.showFees && (
+                        <span className="text-muted caption">
+                            ${parseFloat(ticketPrice).toFixed(2)} + ${parseFloat(
+                                ticketFee
+                            ).toFixed(2)} Fees
+                        </span>
+                    )}
+                </div>
+            </div>
+        </ListGroup.Item>
+    );
 }
--- frontend/src/pages/TicketsPage/TicketsPage.js
+++ frontend/src/pages/TicketsPage/TicketsPage.js
@@ -1,87 +1,88 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { getEvent, getEventTickets, getResaleTickets, getListingsByEvent, getAllEventTickets } from '../../utilities/api';
 import TicketContext from '../../context/Ticket/Ticket';
+
+import { getEvent, getEventTickets, getResaleTickets, getListingsByEvent, getAllEventTickets } from '../../utilities/api';
 
 import { Event, PurchaseTickets } from '../../components';
 
 export default function TicketsPage() {
-	let { id } = useParams();
+    let { id } = useParams();
 
-	const [
-		event,
-		setEvent
-	] = useState();
-	const [
-		tickets,
-		setTickets
-	] = useState();
-	const [
-		listings,
-		setListings
-	] = useState();
-	const [
-		generalAdmissionCount,
-		setGaCount
-	] = useState();
-	const [
-		generalAdmissionTicket,
-		setGaTicket
-	] = useState();
-	const [
-		reSaleTickets,
-		setResaleTickets
-	] = useState();
+    const [
+        event,
+        setEvent
+    ] = useState();
+    const [
+        tickets,
+        setTickets
+    ] = useState();
+    const [
+        listings,
+        setListings
+    ] = useState();
+    const [
+        generalAdmissionCount,
+        setGaCount
+    ] = useState();
+    const [
+        generalAdmissionTicket,
+        setGaTicket
+    ] = useState();
+    const [
+        reSaleTickets,
+        setResaleTickets
+    ] = useState();
 
-	useEffect(
-		() => {
-			// getEventTickets(id)
-			// 	.then((res) => {
-			// 		setTickets(res.data.data);
-			// 		let generalAdminssionCount = res.data.data.map(
-			// 			(ticket) =>
-			// 				ticket.attributes.generalAdmission === true &&
-			// 				ticket.attributes.on_sale_status === 'available'
-			// 		).length;
-			// 		let generalAdmissionTicket = res.data.data.find(
-			// 			(ticket, index) =>
-			// 				ticket.attributes.generalAdmission === true &&
-			// 				ticket.attributes.on_sale_status === 'available'
-			// 		);
-			// 		setGaCount(generalAdminssionCount);
-			// 		setGaTicket(generalAdmissionTicket);
-			// 	})
-			// 	.catch((err) => console.error(err));
+    useEffect(
+        () => {
+            // getEventTickets(id)
+            // 	.then((res) => {
+            // 		setTickets(res.data.data);
+            // 		let generalAdminssionCount = res.data.data.map(
+            // 			(ticket) =>
+            // 				ticket.attributes.generalAdmission === true &&
+            // 				ticket.attributes.on_sale_status === 'available'
+            // 		).length;
+            // 		let generalAdmissionTicket = res.data.data.find(
+            // 			(ticket, index) =>
+            // 				ticket.attributes.generalAdmission === true &&
+            // 				ticket.attributes.on_sale_status === 'available'
+            // 		);
+            // 		setGaCount(generalAdminssionCount);
+            // 		setGaTicket(generalAdmissionTicket);
+            // 	})
+            // 	.catch((err) => console.error(err));
 
 
-			// getResaleTickets(id).then((res) => setResaleTickets(res.data.data)).catch((err) => console.error(err));
+            // getResaleTickets(id).then((res) => setResaleTickets(res.data.data)).catch((err) => console.error(err));
 
-			// getListingsByEvent(id).then((res) => setListings(res.data)).catch((err) => console.error(err));
-		},
-		[
-			id
-		]
-	);
+            // getListingsByEvent(id).then((res) => setListings(res.data)).catch((err) => console.error(err));
+        },
+        [
+            id
+        ]
+    );
 
-	useEffect(() => {
-		getAllEventTickets(id)
-			.then((res) => {
-				// console.log('TicketPage Data ', res.data)
-				setTickets(res.data?.tickets)
-				setEvent(res.data?.event)
-			})
-			.catch((err) => console.error(err))
-	}, [id])
+    useEffect(() => {
+        getAllEventTickets(id)
+            .then((res) => {
+                // console.log('TicketPage Data ', res.data)
+                setTickets(res.data?.tickets)
+                setEvent(res.data?.event)
+            })
+            .catch((err) => console.error(err))
+    }, [id])
 
-	return (
-		<div className="full-height-wrapper">
-			<TicketContext.Provider value={{ tickets, listings, generalAdmissionCount, generalAdmissionTicket, reSaleTickets }}>
-				<div className="pt-md-3">
-					<Event event={event} />
-				</div>
-				<PurchaseTickets />
-			</TicketContext.Provider>
-		</div>
-	);
+    return (
+        <div className="full-height-wrapper">
+            <TicketContext.Provider value={{ tickets, listings, generalAdmissionCount, generalAdmissionTicket, reSaleTickets }}>
+                <div className="pt-md-3">
+                    <Event event={event} />
+                </div>
+                <PurchaseTickets />
+            </TicketContext.Provider>
+        </div>
+    );
 }
