2b7674af24bc5790266d77c6f4b5b58e49113cf8
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-09-07 11:44:45 -0400

    Venue default select

--- admin/src/components/BasicInfoWrapper/Location/Location.js
+++ admin/src/components/BasicInfoWrapper/Location/Location.js
@@ -8,7 +8,7 @@ export default function Location({ event, handleChange, timezoneOpt, venues }) {
             <Form.Group className="form-group" controlId="venue">
                 <Form.Label>Venue</Form.Label>
                 <Form.Select name="venue" value={event?.venue?.id} onChange={handleChange}>
-                    <option key="bad" disabled value="" hidden>Select venue</option>
+                    <option key="-1">Select venue</option>
                     {venues && venues.map((option, index) => (
                         <option key={index} value={option?.id}>{option?.name} - {option?.address[0]?.address_1} {option?.address[0]?.city}, {option?.address[0]?.state}</option>
                     ))}
