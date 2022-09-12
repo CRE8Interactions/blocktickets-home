94568723450696eb477de231aa0250e66f2a2fa2
Jaime Convery <jaimeconvery@gmail.com>
2022-09-12 12:06:11 -0400

    fix: get state from child components for sign up to work

--- admin/src/components/BankAccountDetailsWrapper/BankAccountDetailsWrapper.js
+++ admin/src/components/BankAccountDetailsWrapper/BankAccountDetailsWrapper.js
@@ -4,7 +4,7 @@ import { isMatching, stateOpt } from '../../utilities/helpers';
 
 import { BankAccountDetails } from './BankAccountDetails'
 
-export default function BankAccountDetailsWrapper({ getBankAccount, isValid, setIsValid, submitBankAccount }) {
+export default function BankAccountDetailsWrapper({ getBankAccount, isValid, setIsValid }) {
 
     const inputEl = useRef();
 
@@ -42,8 +42,7 @@ export default function BankAccountDetailsWrapper({ getBankAccount, isValid, set
 
     // update parent state when state changes 
     useEffect(() => {
-        // getBankAccount(bankAccount)
-        submitBankAccount(bankAccount)
+        getBankAccount(bankAccount)
     }, [bankAccount])
 
     // reset error when accountNumber input changed
