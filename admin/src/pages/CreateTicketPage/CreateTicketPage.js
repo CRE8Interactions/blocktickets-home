0b938e0010dfb06651232955b60d102880816494
Jaime Convery <jaimeconvery@gmail.com>
2022-09-16 17:39:43 -0400

    refactor: pass event prop to access event

--- admin/src/pages/CreateTicketPage/CreateTicketPage.js
+++ admin/src/pages/CreateTicketPage/CreateTicketPage.js
@@ -1,4 +1,7 @@
+import React, { useState, useEffect } from 'react';
 import { useSearchParams, useParams } from "react-router-dom";
+
+import { getEvent } from '../../utilities/api';
 
 import { CreateTicketWrapper } from "../../components";
 
@@ -6,11 +9,20 @@ export default function CreateTicketPage() {
 
     // get type query from URL
     const [searchParams] = useSearchParams();
+
     const type = searchParams.get("type")
 
     const { uuid } = useParams()
 
+    const [event, setEvent] = useState();
+
+    useEffect(() => {
+        getEvent(uuid)
+            .then((res) => setEvent(res.data))
+            .catch((err) => console.error(err))
+    }, [uuid])
+
     return (
-        <CreateTicketWrapper eventId={uuid} type={type} />
+        <CreateTicketWrapper eventId={uuid} type={type} event={event} />
     )
 }
