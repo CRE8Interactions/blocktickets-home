1854a1d2af411ab8de8d3b829811478435963e0f
Jaime Convery <jaimeconvery@gmail.com>
2022-09-29 15:14:53 -0400

    refactor: change code

--- frontend/src/components/Card/MyCard.js
+++ frontend/src/components/Card/MyCard.js
@@ -11,7 +11,7 @@ export default function MyCard({ prefix, venue, event }) {
 
     return (
         <Card className="myCard">
-            {prefix === 'event' ? <Card.Img variant="top" src={event?.image?.url} width="100" height="180" /> : <Card.Img variant="top" src={venue?.image[0]?.url} width="100" height="180" />}
+            <Card.Img variant="top" src={prefix == 'event' ? event?.image?.url : venue?.image[0]?.url} width="254" height="303" />
             <Card.Body>
                 {prefix === 'event' ? <EventCardBody event={event} /> : <VenueCardBody venue={venue} />}
             </Card.Body>
