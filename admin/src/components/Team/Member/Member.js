d4b052a9f64cf23e6cc38faf939440a38afce863
Jaime Convery <jaimeconvery@gmail.com>
2022-09-30 11:31:55 -0400

    refactor: revert team management commit

--- admin/src/components/Team/Member/Member.js
+++ admin/src/components/Team/Member/Member.js
@@ -10,7 +10,7 @@ import { EditDeleteDropdown } from '../../EditDeleteDropdown';
 export default function Member({ member, handleShowAdd, handleShowDelete }) {
     return (
         <Stack direction="horizontal" as="li" className='list-item split-row'>
-            <h2 className='normal m-0'>{formatString(`${member?.firstName} ${member?.lastName}`)}</h2>
+            <h2 className='normal m-0'>{formatString(member?.name)}</h2>
             <Stack direction='horizontal' gap={2}>
                 <Badge bg={`${member?.role?.name === 'Admin' ? 'success' : 'default'}`} className={`text-uppercase ${member?.role?.name !== 'Admin' ? 'badge-outline badge-outline--dark' : ''}`}>{formatString(member?.role?.name)}</Badge>
                 {member.role !== 'master_admin' && (
