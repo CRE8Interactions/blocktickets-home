1fbaa9601eed7e5d335215f222f24bb2ee7924a1
Jaime Convery <jaimeconvery@gmail.com>
2022-09-30 15:38:44 -0400

    feat: hook up user permissions

--- admin/src/components/DashboardWrapper/DashboardWrapper.js
+++ admin/src/components/DashboardWrapper/DashboardWrapper.js
@@ -3,18 +3,36 @@ import React from 'react';
 import { Reports } from '../Reports';
 import { OrdersTable } from '../OrdersTable';
 import { Cards } from './Cards';
+import { NoPermissionsContainer } from '../NoPermissionsContainer';
 
 export default function DashboardWrapper({ stats, setRange }) {
+
+    // const user = useContext(UserContext);
+
+    let hasPermission = false;
+
+    // useEffect(() => {
+    //   hasPermission = user.permissions.includes['view_dashboard']
+    // }, [])
+
+
     return (
-        <div className='max-width-wrapper'>
-            <Reports title="dashboard" stats={stats} setRange={setRange} />
-            <Cards stats={stats} />
-            {/* <section>
-                <header className="section-header section-heading">
+        <>
+            <div className="position-relative">
+                <div className={`max-width-wrapper ${!hasPermission ? 'overlay' : ''}`}>
+                    <Reports title="dashboard" stats={stats} setRange={setRange} />
+                    <Cards stats={stats} />
+                    {/* <section>
+                    <header className="section-header section-heading">
                     <h1>Orders</h1>
-                </header>
-                <OrdersTable />
-            </section> */}
-        </div>
+                    </header>
+                    <OrdersTable />
+                </section> */}
+                </div>
+                {!hasPermission && (
+                    <NoPermissionsContainer />
+                )}
+            </div>
+        </>
     );
 }
