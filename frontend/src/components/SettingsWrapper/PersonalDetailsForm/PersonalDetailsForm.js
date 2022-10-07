15b3a5635215076efd662c3b9952dbeb867650e4
Jaime Convery <jaimeconvery@gmail.com>
2022-10-07 13:56:59 -0400

    fix: hide gender and dob fields

--- frontend/src/components/SettingsWrapper/PersonalDetailsForm/PersonalDetailsForm.js
+++ frontend/src/components/SettingsWrapper/PersonalDetailsForm/PersonalDetailsForm.js
@@ -1,155 +1,168 @@
 import React, { Fragment, useState, useEffect } from 'react';
 
+import { updatePersonalDetails } from '../../../utilities/api';
+import authService from '../../../utilities/services/auth.service';
+
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import { updatePersonalDetails } from '../../../utilities/api';
-import authService from '../../../utilities/services/auth.service';
-
 export default function PersonalDetailsForm() {
-	let user = sessionStorage.getItem('user');
-	user = JSON.parse(user).user;
+    let user = sessionStorage.getItem('user');
+    user = JSON.parse(user).user;
 
-	const [
-		formValid,
-		setFormValid
-	] = useState(false);
+    const [initialState, setInitialState] = useState()
 
-	const [
-		formData,
-		setFormData
-	] = useState({});
+    const [
+        formValid,
+        setFormValid
+    ] = useState(false);
 
-	const [
-		firstName,
-		setFirstName
-	] = useState(user.firstName);
+    const [
+        formData,
+        setFormData
+    ] = useState({});
 
-	const [
-		lastName,
-		setLastName
-	] = useState(user.lastName);
+    const [
+        firstName,
+        setFirstName
+    ] = useState(user.firstName);
 
-	const [
-		email,
-		setEmail
-	] = useState(user.email);
+    const [
+        lastName,
+        setLastName
+    ] = useState(user.lastName);
 
-	const [
-		dob,
-		setDob
-	] = useState(user.dob);
+    const [
+        email,
+        setEmail
+    ] = useState(user.email);
 
-	const [
-		gender,
-		setGender
-	] = useState(user.gender);
+    const [disabled, setDisabled] = useState(true);
 
-	const [show, setShow] = useState(false);
+    // const [
+    // 	dob,
+    // 	setDob
+    // ] = useState(user.dob);
 
-	useEffect(
-		() => {
-			if (firstName && lastName && email && gender && dob) {
-				setFormValid(true);
-			}
-			else {
-				setFormValid(false);
-			}
-		},
-		[
-			firstName,
-			lastName,
-			email,
-			dob,
-			gender
-		]
-	);
+    // const [
+    // 	gender,
+    // 	setGender
+    // ] = useState(user.gender);
 
-	useEffect(() => {
-		// set default value for gender dropdown
-		let select = document.getElementById('gender');
-		for (let i = 0; i < select.options.length; i++) {
-			if (select.options[i].value === user.gender) select.options[i].setAttribute('selected', true);
-		}
-		if (firstName && lastName && email && gender && dob) {
-			setFormValid(true);
-		}
-		else {
-			setFormValid(false);
-		}
-	}, []);
+    const [show, setShow] = useState(false);
 
-	const submitForm = () => {
-		let data = {
-			data: {
-				dob,
-				email,
-				firstName,
-				lastName,
-				gender
-			}
-		};
+    useEffect(
+        () => {
+            if (firstName && lastName && email) {
+                setFormValid(true);
+            }
+            else {
+                setFormValid(false);
+            }
+        },
+        [
+            firstName,
+            lastName,
+            email
+        ]
+    );
 
-		updatePersonalDetails(data).then((res) => {
-			authService.setUser(res.data); 
-			setShow(true)
-		}).catch((err) => console.error(err));
-	};
+    useEffect(() => {
+        if (formValid && (initialState?.email !== email || initialState?.firstName !== firstName || initialState?.lastName !== lastName)) {
+            setDisabled(false)
+        }
+    }, [formValid, firstName, lastName, email])
 
-	const notificationModal = () => {
-		if (show) {
-			return (
-				<Alert variant="success" onClose={() => setShow(false)} dismissible>
-					<p>
-						Your Personal Details have successfully been updated.  
-					</p>
-				</Alert>
-			);
-		}
-	}
+    useEffect(() => {
+        setInitialState({
+            firstName,
+            lastName,
+            email
+        })
+        // set default value for gender dropdown
+        // let select = document.getElementById('gender');
+        // for (let i = 0; i < select.options.length; i++) {
+        //     if (select.options[i].value === user.gender) select.options[i].setAttribute('selected', true);
+        // }
+        if (firstName && lastName && email) {
+            setFormValid(true);
+        }
+        else {
+            setFormValid(false);
+        }
+    }, []);
 
-	return (
-		<Fragment>
-			<Form className="d-flex-column">
-				{ notificationModal() }
-				<Form.Group className="form-group" controlId="email">
-					<Form.Label>Email</Form.Label>
-					<Form.Control
-						type="email"
-						placeholder="Enter your email"
-						required
-						name="email"
-						defaultValue={user.email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</Form.Group>
+    const submitForm = () => {
+        let data = {
+            data: {
+                email,
+                firstName,
+                lastName
+            }
+        };
 
-				<Form.Group className="form-group" controlId="name">
-					<Form.Label>First Name</Form.Label>
-					<Form.Control
-						type="text"
-						placeholder="Enter your first name"
-						required
-						name="firstName"
-						defaultValue={user.firstName}
-						onChange={(e) => setFirstName(e.target.value)}
-					/>
-				</Form.Group>
-				<Form.Group className="form-group" controlId="name">
-					<Form.Label>Last Name</Form.Label>
-					<Form.Control
-						type="text"
-						placeholder="Enter your last name"
-						required
-						name="lastName"
-						defaultValue={user.lastName}
-						onChange={(e) => setLastName(e.target.value)}
-					/>
-				</Form.Group>
-				<Row className="form-group">
+        updatePersonalDetails(data).then((res) => {
+            authService.setUser(res.data);
+            setShow(true)
+        }).catch((err) => {
+            console.error(err)
+        });
+    };
+
+    const notificationModal = () => {
+        if (show) {
+            return (
+                <Alert variant="success" onClose={() => setShow(false)} dismissible>
+                    <p>
+                        Your Personal Details have successfully been updated.
+                    </p>
+                </Alert>
+            );
+        }
+    }
+
+    return (
+        <Fragment>
+            <Form className="d-flex-column">
+                {notificationModal()}
+                <Form.Group className="form-group" controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter your email"
+                        required
+                        name="email"
+                        defaultValue={user.email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+
+                <Form.Group className="form-group" controlId="name">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter your first name"
+                        required
+                        name="firstName"
+                        defaultValue={user.firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group className="form-group" controlId="name">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter your last name"
+                        required
+                        name="lastName"
+                        defaultValue={user.lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </Form.Group>
+                {/* <Row className="form-group">
 					<Col>
 						<Form.Group className="form-group" controlId="dob">
 							<Form.Label>Birth Date</Form.Label>
@@ -177,11 +190,11 @@ export default function PersonalDetailsForm() {
 							</Form.Select>
 						</Form.Group>
 					</Col>
-				</Row>
-				<Button disabled={!formValid} size="lg" onClick={(e) => submitForm()}>
-					Update
-				</Button>
-			</Form>
-		</Fragment>
-	);
+				</Row> */}
+                <Button disabled={disabled} size="lg" onClick={(e) => submitForm()}>
+                    Update
+                </Button>
+            </Form>
+        </Fragment>
+    );
 }
