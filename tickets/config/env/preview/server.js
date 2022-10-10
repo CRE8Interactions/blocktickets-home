ded0b5bc70ceb1e5c87558a497f4775d7e1942e3
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-10-10 19:06:58 -0400

    preview settings update

---
+++ tickets/config/env/preview/server.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('URL', 'https://preview.api.blocktickets.xyz'),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  proxy: env.bool('IS_PROXIED', true),
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
+  },
+});
