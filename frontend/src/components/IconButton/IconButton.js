1bc857e8a3ad34fc0aff3e0308bea5eb04ef46de
Jaime Convery <jaimeconvery@gmail.com>
2022-10-11 16:22:07 -0400

    fix: external link

--- frontend/src/components/IconButton/IconButton.js
+++ frontend/src/components/IconButton/IconButton.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function IconButton({ children, styles, variant, btn = '', size, link, relative }) {
-	return (
-		<Link
-			to={link}
-			relative={relative}
-			className={`btn btn-${variant} ${btn && `btn-${btn}`} icon-button ${styles && styles} ${size
-				? `btn-${size}`
-				: ''} `}>
-			{children}
-		</Link>
-	);
+export default function IconButton({ children, styles, variant, btn = '', size, link, isExternal = false }) {
+
+    return (
+        <>
+            {isExternal ? (
+                <a href={link} className={`btn btn-${variant} ${btn && `btn-${btn}`} icon-button ${styles && styles} ${size
+                    ? `btn-${size}`
+                    : ''} `}>{children}</a>
+            ) : (
+                <Link
+                    to={link}
+                    className={`btn btn-${variant} ${btn && `btn-${btn}`} icon-button ${styles && styles} ${size
+                        ? `btn-${size}`
+                        : ''} `}>
+                    {children}
+                </Link>
+            )}
+        </>
+    );
 }
