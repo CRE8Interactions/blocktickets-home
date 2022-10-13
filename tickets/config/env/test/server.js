d72530b4554f34b491f58440c46cbd50e508c782
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-10-13 07:36:02 -0400

    Cron task update

--- tickets/config/env/test/server.js
+++ tickets/config/env/test/server.js
@@ -1,4 +1,4 @@
-const cronTasks = require("./cron-tasks");
+const cronTasks = require("../../cron-tasks");
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
@@ -9,6 +9,6 @@ module.exports = ({ env }) => ({
   },
   cron: {
     enabled: env("CRON_ENABLED", false),
-    // tasks: cronTasks,
+    tasks: cronTasks,
   },
 });
