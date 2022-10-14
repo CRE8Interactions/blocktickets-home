146bc6755a622a7f5822d2bd2994a0bc9ebf3787
Jaime Convery <jaimeconvery@gmail.com>
2022-10-14 14:35:43 -0400

    refactor: change spelling

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -23,7 +23,7 @@ export default function Hero() {
         <header className="hero">
             <div className="d-flex-column align-items-center">
                 <h1 className='display-1'>The future of ticketing is here</h1>
-                <h2 className='subtitle'>Get your NFT tickets for live events and discover unforgettable moments from your favourite artists</h2>
+                <h2 className='subtitle'>Get your NFT tickets for live events and discover unforgettable moments from your favorite artists</h2>
             </div>
             <div className="hero-search" onClick={handleClick}>
                 <SearchBar size="lg" showMobile={false} open={show} showBtn={true} placeholder="Search for events near you" />
