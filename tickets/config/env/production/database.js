f836802d3036b38fa1ca8c630b6f47626808a69b
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2022-10-15 20:21:46 -0400

    Prod db config

--- tickets/config/env/production/database.js
+++ tickets/config/env/production/database.js
@@ -5,19 +5,18 @@ const { host, port, database, user, password } = parse(
 );
 
 module.exports = ({ env }) => ({
-  connection: {
+  connection: {  
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host,
+      port,
+      database,
+      user,
+      password,
       ssl: {
-        ca: env('DATABASE_CA')
+        rejectUnauthorized: false,
       },
     },
-    debug: false,
+      debug: false,
   },
 });
