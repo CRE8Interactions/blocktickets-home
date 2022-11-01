d7183f83c27953a2a4bfe78e5004a3ca1b410487
Jaime Convery <jaimeconvery@gmail.com>
2022-11-01 16:40:06 -0400

    style: style edits

--- info/src/components/Join/Join.js
+++ info/src/components/Join/Join.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
-
 import './cta.scss';
 
 export default function Join() {
     return (
-        <section className='cta d-flex-column text-center'>
-            <div className="section-header">
+        <section className='section-lg text-center'>
+            <div className="mb-4">
                 <h1 className='section-heading-sm'>Want to join our mission?</h1>
                 <h2 className="subtitle">We're always looking to add to the team! Feel free to reach out and see what positions are available.</h2>
-
             </div>
+            <Link className='btn btn-outline-light btn-lg'>Get in tounch</Link>
         </section>
     )
 }
